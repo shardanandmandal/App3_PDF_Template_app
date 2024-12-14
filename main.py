85aa9f0077662554b7352ccbcd1f9f5956ec5f45
shardanandmandal <shardanandmandal@gmail.com>
2024-12-14 21:11:17 +0530

    Create PDf from CSV multiple pages with footer

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf=FPDF(orientation="P",unit="mm",format="A4")
-
+pdf.set_auto_page_break(auto=False)
 df=pd.read_csv("topics.csv")
 
 for index,row in df.iterrows():
@@ -13,8 +13,18 @@ for index,row in df.iterrows():
     pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1)
     pdf.line(10,20,200,20)
 
+    pdf.ln(265)
+    pdf.set_font(family="Times",style="I",size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0,h=12,txt=row["Topic"],align="R",ln=1)
+
+
     for r in range(row["Pages"]-1):
         pdf.add_page()
+        pdf.ln(275)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
 
 
 
